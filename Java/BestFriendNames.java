7d060713258c40e71a149939fd88bdaa0b2d26eb
anandkrishnapindiprolu <anandakrishnapindiprolu5896@gmail.com>
2023-05-16 00:17:52 +0530

    BestFriendsNames

---
+++ Java/BestFriendNames.java
@@ -0,0 +1,12 @@
+import java.io.*;
+class BestFriendNames
+{
+	public static void main(String args[])
+	{
+		System.out.print("\n Prudhvi");
+		System.out.print("\n Sai");
+		System.out.print("\n Leela");
+		System.out.print("\n Shalu");
+		System.out.print("\n Preethi");
+	}
+}
